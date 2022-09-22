2471d26520b6d0f2895fe9830bd46c2eb7f4eff5
Luciano Lucero <50858818+LucLucero@users.noreply.github.com>
2022-09-22 06:44:12 -0300

    rev2
    rev2

--- PokeDex/scripts/scripts.js
+++ PokeDex/scripts/scripts.js
@@ -5,6 +5,25 @@ async function fetchData(pokeNumber) {
     return pokemon;
 }
 
+let grassType = '#74CB48';
+let poisonType = '#A43E9E';
+let bugType = '#A7B723';
+let dragonType = '#F5FFC6';
+let electricType = '#F9CF30';
+let fairyType = '#B899FF';
+let fightingType = '#C9A97E';
+let fireType = '#F57D31';
+let flyingType = '#A891EC';
+let ghostType = '#70559B';  
+let groundType = '#906D3C';
+let iceType = '#CCDAD1';
+let normalType = '#AAA67F';
+let psychicType = '#FB5584';
+let rockType = '#B69E31';
+let steelType = '#B7B9D0';
+let waterType = '#6493EB';
+
+
 async function createPokemon(pokeNumber){
 
 
@@ -15,22 +34,34 @@ async function createPokemon(pokeNumber){
 
     const container = document.getElementById('container');
     const content = document.getElementById('content');
+    const pokemonBox = document.getElementById('pokemon-box');
     const div = document.createElement('div');
-    const h2 = document.createElement('h2');
+    div.setAttribute("id", "poke");
+    const h2Name = document.createElement('h2');
+    const h3Id = document.createElement('h3');
     const img = document.createElement('img');
+    const borderColor = div.style.borderColor = ghostType;
+    const tipo = document.createElement('p');
 
     //conteudo dos elementos
+   // borderColor = pokemon.ty
 
-    h2.textContent = `#${pokemon.id} - ${pokemon.name}`;
+    
+    tipo.textContent = `${pokemon.type}`;
+    h2Name.textContent = `${pokemon.name}`;
+    h3Id.textContent = `#${pokemon.id}`;
     img.src = pokemon.sprites.front_default;
 
     // Adicionando os elementos no html
 
-    div.appendChild(h2);
+    div.appendChild(tipo);
+    div.appendChild(h2Name);
+    div.appendChild(h3Id);
     div.appendChild(img);
-    content.appendChild(div);
+    //div.appendChild(borderColor);
     
-
+    pokemonBox.appendChild(div);
+    
 }
 
 
